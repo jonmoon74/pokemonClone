1f065aa036e8ccc71dd2d3fcb239ea756eaee40e
jonmoon74 <jon.moon@outlook.com>
2018-08-27 20:58:31 +0100

    mapview zoom to initial location set

--- PokemonClone/ViewController.swift
+++ PokemonClone/ViewController.swift
@@ -9,22 +9,35 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
     @IBOutlet weak var mapView: MKMapView!
     
     var manager = CLLocationManager()
-    
+    var updateCount = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        mapView.showsUserLocation = true
+        manager.delegate = self
         
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            print ("Ready to go")
+            mapView.showsUserLocation = true
+            manager.startUpdatingLocation()
+            
+        } else {
+            manager.requestWhenInUseAuthorization()
+        }
         
     }
-
-   
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if updateCount < 5 {
+            let region = MKCoordinateRegionMakeWithDistance(manager.location!.coordinate, 500, 500)
+            mapView.setRegion(region, animated: false)
+            updateCount += 1
+        }
+    }
 }
 
